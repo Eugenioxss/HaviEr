dc386cb40a88f335324f9f0b195dc97109ea1a52
Commodore <moreugenio@gmail.com>
2026-04-25 17:25:05 -0600

    cambio de modelo

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ CORS(app) # Importante para que tu Frontend no tenga errores de conexión
 
 # Configuración de Gemini
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.0-flash-lite')
 
 @app.route('/api/chat-havi', methods=['POST'])
 def chat_havi():
